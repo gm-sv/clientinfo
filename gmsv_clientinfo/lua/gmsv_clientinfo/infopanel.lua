1ece247d5742c833da89db6adfc26c1c6f694cca
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2024-08-09 01:45:01 -0400

    Put everything into SlowTick

--- gmsv_clientinfo/lua/gmsv_clientinfo/infopanel.lua
+++ gmsv_clientinfo/lua/gmsv_clientinfo/infopanel.lua
@@ -3,47 +3,51 @@ local GetHostName = GetHostName
 local RealFrameTime = RealFrameTime
 local engine_ServerFrameTime = engine.ServerFrameTime
 local math_Clamp = math.Clamp
+local math_floor = math.floor
+local math_min = math.min
+local ScrW = ScrW
+local ScrH = ScrH
 
 local PANEL = {}
 
 AccessorFunc(PANEL, "m_iMaxTickrate", "MaxTickrate", FORCE_NUMBER)
 AccessorFunc(PANEL, "m_iTickCounter", "TickCounter", FORCE_NUMBER)
-AccessorFunc(PANEL, "m_iFramerate", "Framerate", FORCE_NUMBER)
 
 function PANEL:Init()
-	self:SetMaxTickrate(math.floor(1 / engine.TickInterval()))
+	self:SetMaxTickrate(math_floor(1 / engine.TickInterval()))
 	self:SetTickCounter(0)
-	self:SetFramerate(math.floor(1 / RealFrameTime()))
 
 	self:ParentToHUD()
-	self:SetSize(300, 20)
-	self:SetPos(ScrW() - 300, ScrH() - 20)
 
 	local InfoLabel = vgui.Create("DLabel", self)
 	InfoLabel:SetFont("BudgetLabel")
 	self.m_InfoLabel = InfoLabel
 
+	-- They say you shouldn't do this, too bad!
 	hook.Add("Tick", self, self.Tick)
 end
 
 function PANEL:SlowTick()
-	self:SetFramerate(math.floor(1 / RealFrameTime()))
-end
-
-function PANEL:Tick()
 	local MaxTickrate = self:GetMaxTickrate()
 	local EstimatedTickrate = math_Clamp(1 / engine_ServerFrameTime(), 0, MaxTickrate) -- Since it's an estimate it goes out of bounds a lot
-	local Framerate = self:GetFramerate()
+	local Framerate = math_floor(1 / RealFrameTime())
 
 	self.m_InfoLabel:SetText(Format("%s    TPS: %u / %u    FPS: %u", GetHostName(), EstimatedTickrate, MaxTickrate, Framerate))
 	self.m_InfoLabel:SizeToContents()
 
-	self:SetSize(self.m_InfoLabel:GetSize())
+	-- Might stutter around a bit
+	local Width, Height = self.m_InfoLabel:GetSize()
+	With = math_min(Width, 300) -- Don't let it squish too much. Ideally the spacing would squish but that's a bit much work for this little panel :]
 
-	-- SlowTick
+	self:SetSize(Width, Height)
+	self:SetPos(ScrW() - (Width + 12), ScrH() - Height)
+end
+
+function PANEL:Tick()
 	self:SetTickCounter(self:GetTickCounter() + 1)
 
-	if self:GetTickCounter() >= MaxTickrate then
+	-- SlowTick every half second
+	if self:GetTickCounter() >= (self:GetMaxTickrate() * 0.5) then
 		self:SlowTick()
 		self:SetTickCounter(0)
 	end
